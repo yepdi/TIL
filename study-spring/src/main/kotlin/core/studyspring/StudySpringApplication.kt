7ea01133cb2d299349545c4ea822b7c3fe3be4ab
yepdi <kyj9303@gmail.com>
2022-07-31 17:18:46 +0900

    [study-spring] project initialization

---
+++ study-spring/src/main/kotlin/core/studyspring/StudySpringApplication.kt
@@ -0,0 +1,11 @@
+package core.studyspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudySpringApplication>(*args)
+}
---
+++ study-spring/src/test/kotlin/core/studyspring/StudySpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package core.studyspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
